f0a2d25005f15b812d094d09706f63461b5d2877
ChrisMac89 <Chris_mac89@hotmail.co.uk>
2019-10-27 00:59:13 +0100

    Question for the quiz #1amMadness

---
+++ pages/quizzes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- This is a basic title with question, and a submission box, Need zohaib to update css document -->
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+    </script>   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container">
+    <h2>Daily Question</h2>
+    <div class="card">
+        <div class="card-header">Whats demotivates you when you've been working out?</div>
+        <div class="card-body"><div class="input-group">
+
+                <span class="input-group-btn"><button class="btn btn-info">Failing a workout</button>
+                    <span class="input-group-btn"><button class="btn btn-info">Seeing someone lift more weight than you</button>
+                        <span class="input-group-btn"><button class="btn btn-info">Nothing</button>
+                            <span class="input-group-btn"><button class="btn btn-info">Not liking the way you look</button>
+
+                </span> </div> </div>
+    </div> </div>
+</body>
+</html>
