51bcfc017675ae593e25eb438175da1444919d81
JAParsons <johnparsons932@gmail.com>
2019-10-26 22:26:53 +0100

    added emoji button selectors

---
+++ emoji_options.php
@@ -0,0 +1,29 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Team 1">
+
+    <title> Actify</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+          crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <script src="https://d3js.org/d3.v5.js"></script>
+</head>
+
+<input type="checkbox" name='option1' id="option1"/><label for="option1"></label>
+<input type="checkbox" name='option2' id="option2"/><label for="option2"></label>
+<input type="checkbox" name='option3' id="option3"/><label for="option3"></label>
+<input type="checkbox" name='option4' id="option4"/><label for="option4"></label>
+<input type="checkbox" name='option5' id="option5"/><label for="option5"></label>
