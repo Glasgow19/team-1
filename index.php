73f34d7a70ce6814c3e361d73246919227e0d05d
Zohaib Nawaz Tarar <zohaib_nawaz786@hotmail.com>
2019-10-26 15:33:17 +0100

    Added basic bootstrap template

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Team 8">
+
+    <title>Compare Care</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+          crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <script src="https://d3js.org/d3.v5.js"></script>
+</head>
+
+<body>
+
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#"><i class="fas fa-running"
+                                        title="Logo"></i>
+        Company Name Here</a>
+    <button class="navbar-toggler my-1" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="faq.php">FAQs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact-us.php">Contact Us</a>
+            </li>
+        </ul>
+        <form class="navbar-form form-inline" action="login.php" method="POST">
+            <div id="id3" class="form-group">
+                <input readonly="true" id="id3.1" class="form-control mr-2" type="text" placeholder="Username"
+                       name="userName" required aria-label="Enter your username">
+                <input readonly="true" id="id3.2" class="form-control mr-2" type="password" placeholder="Password"
+                       name="uncrypPass" required aria-label="Enter your Password">
+                <input class="form-control" type="hidden" name="remember" value="checked">
+                <button class="btn btn-outline-success login-btn my-2 my-sm-0 mr-2" type="submit">Log In</button>
+                <button class="btn btn-outline-success login-btn my-2 my-sm-0 mr-2" type="register"
+                        onclick="location.href='register.php';">Register
+                </button>
+            </div>
+        </form>
+        <div id="id4" class="logout">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link account" href="dashboard.php">
+
+                        Account</a>
+                </li>
+                <li class="nav-item">
+                    <form action="login.php" method="POST">
+                        <input type='hidden' name="logout" value="true">
+                        <button class="btn btn-outline-success login-btn my-2 my-sm-0 mr-2" type="submit">Logout
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Header with Background Image -->
+<header class="header" role="banner">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="float-lg-left text-center">
+                    <br><br>
+                    <h1 class="mt-5">Some text here</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</header>
+
+
+<!-- Page Content -->
+<!-- --------------------------------------------------------------------- -->
+
+
+
+<!-- Footer -->
+<footer class="py-3 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">&copy; Company Name Here 2019</p>
+    </div>
+    <!-- /.container -->
+</footer>
+<!-- Bootstrap core JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
