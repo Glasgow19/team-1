bd837d2540f603602a2e62c8feaf58a1c96f684b
JAParsons <johnparsons932@gmail.com>
2019-10-27 04:28:04 +0000

    fixed password bug

--- php functions/login_register.php
+++ php functions/login_register.php
@@ -47,7 +47,7 @@ if (isset($_POST['reg_user'])) {
     // Finally, register user if there are no errors in the form
     if (count($errors) == 0) {
         //$password = md5($password_1);//encrypt the password before saving in the database
-
+        $password = $password_1;
         $query = "INSERT INTO users (email_address, password, age, gender, location ) 
   			  VALUES('$username', '$password', '$age', '$gender', '$location')";
         mysqli_query($db, $query);
