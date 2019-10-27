2340b9da79e0a16273c2cfa51406d353b5410533
ChrisMac89 <Chris_mac89@hotmail.co.uk>
2019-10-27 09:10:32 +0000

    Fixed link in the about section as it was broken.

--- pages/faq.php
+++ pages/faq.php
@@ -45,7 +45,7 @@
                 <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="about1.php">About Us</a>
+                <a class="nav-link" href="about.php">About Us</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="faq.php">FAQs</a>
